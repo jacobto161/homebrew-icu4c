2068b8fe470e639d90bfb81ba128b928cf71f0d0
Reina To <jtt4457@rit.edu>
2025-01-13 10:25:48 -0500

    74.2 again

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,32 +1,23 @@
-class Icu4c < Formula
+class Icu4cAT74 < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "https://icu.unicode.org/home"
   url "https://github.com/unicode-org/icu/releases/download/release-74-2/icu4c-74_2-src.tgz"
   version "74.2"
-  sha256 "818a80712ed3caacd9b652305e01afc7fa167e6f2e94996da44b90c2ab604ce1"
+  sha256 "68db082212a96d6f53e35d60f47d38b962e9f9d207a74cfac78029ae8ff5e08c"
   license "ICU"
 
-  livecheck do
-    url :stable
-    regex(/^release[._-]v?(\d+(?:[.-]\d+)+)$/i)
-    strategy :git do |tags, regex|
-      tags.map { |tag| tag[regex, 1]&.gsub("-", ".") }.compact
-    end
-  end
-
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "734c09d5285cf68f3c86c09522f6f24734bbe6d7338c4900d89092bcd9fb8fc0"
-    sha256 cellar: :any,                 arm64_ventura:  "953797d46546c570c4fab4e8b2395624ae90acd492f75b68ff99fbd115ccd748"
-    sha256 cellar: :any,                 arm64_monterey: "488714896b377fb1bb09f023e0740235730be9098a2bf70e932cd9d1386d4a4a"
-    sha256 cellar: :any,                 arm64_big_sur:  "d51cb68a129278f56d544937e381c8f1deb5edc8a20c40aeb50c6fee93d8eb1d"
-    sha256 cellar: :any,                 sonoma:         "5e416eb34196809126fe5465b776f355abbd4cee61c28e63d059a8f57e8503a6"
-    sha256 cellar: :any,                 ventura:        "4400e31a217d6ffcd2bd06e26ecbeed768133493bfcc4cb3c9ff9702f480beb6"
-    sha256 cellar: :any,                 monterey:       "56abe168ef97fdee2bc2a8d0634c235b54cb7c2740465ee64053d96cbc755a9b"
-    sha256 cellar: :any,                 big_sur:        "707aa80ac7b79afe6047d25a44b90c46ed15fb6f0c255e0057c7dc812a9ab433"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "714dc987ca9c9b594e6e17c02640f93b49074f81264be16051a93a8f5f674d77"
+    sha256 cellar: :any,                 arm64_sequoia: "d9b0dc95a406d340373bf75d6d013cc53e8815d0c0b550e584b4006a7aba12f0"
+    sha256 cellar: :any,                 arm64_sonoma:  "2216fec75032a12c4a442448ca23fed421eb94374e2e9994e6db0c51eae60ed2"
+    sha256 cellar: :any,                 arm64_ventura: "1870714606c1792b9ca84cb9c07028c745575c8f3219c629f56c27b1b333ce25"
+    sha256 cellar: :any,                 sonoma:        "715c3ec07d1056f609f7b670fa6b8767aa508d4e8e4f93b2ae15a635b77715c4"
+    sha256 cellar: :any,                 ventura:       "81c9ad51d30a5b4d33c4b2a1c89d46d27bc4e6c8f1ffe8f723bf2c8d1bb27901"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99595c8a778c7b3161baed663fb738a28213e9e31797aa558f6c5bb5ab239c3d"
   end
 
-  keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
+  keg_only :versioned_formula
+
+  disable! date: "2025-05-01", because: :versioned_formula
 
   def install
     args = %w[
@@ -37,7 +28,7 @@ class Icu4c < Formula
     ]
 
     cd "source" do
-      system "./configure", *std_configure_args, *args
+      system "./configure", *args, *std_configure_args
       system "make"
       system "make", "install"
     end
@@ -45,10 +36,10 @@ class Icu4c < Formula
 
   test do
     if File.exist? "/usr/share/dict/words"
-      system "#{bin}/gendict", "--uchars", "/usr/share/dict/words", "dict"
+      system bin/"gendict", "--uchars", "/usr/share/dict/words", "dict"
     else
       (testpath/"hello").write "hello\nworld\n"
-      system "#{bin}/gendict", "--uchars", "hello", "dict"
+      system bin/"gendict", "--uchars", "hello", "dict"
     end
   end
 end
