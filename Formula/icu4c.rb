f43d907c7d920ddb6ea4fe8b6a2cfcf41b7a74c3
Reina To <jtt4457@rit.edu>
2025-01-13 10:21:12 -0500

    74.2?

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,8 +1,8 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "https://icu.unicode.org/home"
-  url "https://github.com/unicode-org/icu/releases/download/release-73-2/icu4c-73_2-src.tgz"
-  version "73.2"
+  url "https://github.com/unicode-org/icu/releases/download/release-74-2/icu4c-74_2-src.tgz"
+  version "74.2"
   sha256 "818a80712ed3caacd9b652305e01afc7fa167e6f2e94996da44b90c2ab604ce1"
   license "ICU"
 
